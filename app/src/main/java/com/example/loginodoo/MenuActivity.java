107cb6ed9780f993d58d2cc3bc23303054ee7d34
guillemmv <gmagrina1@institutjaumehuguet.cat>
2023-05-03 11:46:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loginodoo/MenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.loginodoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+    public void onPartnerClick(View v){
+        Log.d("MENU", "partner button clicked");
+        Intent myIntent = new Intent(MenuActivity.this, CustomerListActivity.class);
+        MenuActivity.this.startActivity(myIntent);
+    }
+    public void onSaleOrderClick(View v){
+        Log.d("MENU", "sale order button clicked");
+    }
+}
